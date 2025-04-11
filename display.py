848f4328c5459051d68d96cdc624a88797f5e4de
Administrator <@>
2025-04-11 20:59:48 +0800

    0.1-fix！

--- display.py
+++ display.py
@@ -5,8 +5,8 @@ from dqn128_64_32 import EnhancedDQN as DQN128
 from dqn256_128_64 import EnhancedDQN as DQN256
 from dqn256_128_64_32 import EnhancedDQN as DQN256_2
 
-mod = 256
-path = '256_model812round_34score.pth'
+mod = 128
+path = '128_model480round_30score.pth'
 class AIPlayer:
     def __init__(self):
         pygame.init()
@@ -24,7 +24,7 @@ class AIPlayer:
         elif mod == 512:
             self.model = DQN256_2(self.env.observation_space.shape[0],
                                    self.env.action_space.n)
-        self.model.load_state_dict(torch.load(path))
+        self.model.load_state_dict(torch.load(path, map_location='cpu'))
         self.model.eval()
 
     def play(self):
@@ -41,21 +41,12 @@ class AIPlayer:
 
             next_state, _, done, _, _ = self.env.step(action)
             state = next_state
-            # self.env.render(self.screen)
-            # self.clock.tick(60)
+            self.env.render(self.screen)
+            self.clock.tick(60)
             if done:
-                if len(self.score) < 10:
-                    print(self.env.score)
-                    self.score.append(self.env.score)
-                    state = self.env.reset()
-                else:
-                    n = 0
-                    for i in self.score:
-                        n += i
-                    print(n / len(self.score))
-                    break
-
-
+                print(self.env.score)
+                self.score.append(self.env.score)
+                break
 
 
 if __name__ == "__main__":
